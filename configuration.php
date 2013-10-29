d72242c474146761c41adf82950ff0f18032d567
josemi <jose1189@gmail.com>
2013-10-29 23:32:17 +0100

    Signed-off-by: josemi <jose1189@gmail.com>

---
+++ configuration.php
@@ -0,0 +1,10 @@
+<?php
+	class JConfig{
+		public $dbtype = 'mysqli';
+		public $host = 'localhost';
+		public $user = 'root';
+		public $password = '';
+		public $db = 'bares1';
+	}
+?>
+Cambio realizado
